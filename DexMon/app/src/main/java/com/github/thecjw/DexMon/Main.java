67ec0f1f67ef0901608cad3dc1d874914da0e660
TheCjw <thecjw@live.com>
2016-06-29 10:36:29 +0800

    Update DexMon.

--- DexMon/app/src/main/java/com/github/thecjw/DexMon/Main.java
+++ DexMon/app/src/main/java/com/github/thecjw/DexMon/Main.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.util.Log;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -23,12 +26,28 @@ public class Main implements IXposedHookLoadPackage {
   private String processName = "";
   private Context context;
 
+  // dalvik.system.BaseDexClassLoader$pathList
+  private Field fieldPathList;
+  // [Ldalvik.system.DexPathList$Element;$dexElements
+  private Field fieldDexElements;
+  // dalvik.system.DexPathList$Element
+  private Field fieldDexFile;
+  //
+  private Field fieldmCookie;
+  //
+  private Field fieldmFileName;
+
+  private Method methodGetClassNameList;
+
+
   @Override
   public void handleLoadPackage(LoadPackageParam loadPackageParam)
       throws Throwable {
 
     packageName = loadPackageParam.packageName;
     processName = loadPackageParam.processName;
+
+    // TODO: Load so and patch _Z16dvmOptimizeClassP11ClassObjectb and _Z17dvmUpdateCodeUnitPK6MethodPtt
 
     findAndHookMethod("android.app.Application",
         loadPackageParam.classLoader,
@@ -59,9 +78,73 @@ public class Main implements IXposedHookLoadPackage {
 
             ClassLoader _this = (ClassLoader) param.thisObject;
 
-            Log.d(TAG, String.format("Loader: %s, DexFile: %s", _this.getClass().getName(), dexPath));
+            // Log.d(TAG, String.format("Loader: %s, DexFile: %s", _this.getClass().getName(), dexPath));
+
+            resolveAllClasses(_this);
           }
         });
+  }
+
+  private void resolveAllClasses(ClassLoader classLoader) {
+
+    Log.d(TAG, String.format("Current classloader: %s", classLoader.getClass().getName()));
+
+    try {
+      methodGetClassNameList = getMethod(Class.forName("dalvik.system.DexFile"), "getClassNameList");
+      fieldPathList = getField(Class.forName("dalvik.system.BaseDexClassLoader"), "pathList");
+
+      Object pathList = fieldPathList.get(classLoader);
+      fieldDexElements = getField(pathList.getClass(), "dexElements");
+      Object dexElements = fieldDexElements.get(pathList);
+
+      int dexElementsLength = Array.getLength(dexElements);
+      Log.d(TAG, String.format(" Loaded dex files: %d", dexElementsLength));
+
+      for (int i = 0; i < dexElementsLength; i++) {
+        Object dexElement = Array.get(dexElements, i);
+        fieldDexFile = getField(dexElement.getClass(), "dexFile");
+        Object dexFile = fieldDexFile.get(dexElement);
+
+        fieldmCookie = getField(dexFile.getClass(), "mCookie");
+        Object mCookie = fieldmCookie.get(dexFile);
+
+        fieldmFileName = getField(dexFile.getClass(), "mFileName");
+        String fileName = (String)fieldmFileName.get(dexFile);
+
+        String[] classNames = (String[]) methodGetClassNameList.invoke(dexFile, mCookie);
+        Log.d(TAG, String.format("  %d): %s, Classes: %d", i, fileName, classNames.length));
+
+        // resolve all.
+        for (String className : classNames) {
+          try {
+            classLoader.loadClass(className);
+          } catch (Exception e) {
+            Log.e(TAG, String.format("  Resolve %s failed, classLoader: %s", className, classLoader.getClass().getName()));
+          }
+        }
+      }
+
+    } catch (Exception e) {
+      Log.e(TAG, e.getMessage());
+    }
+  }
+
+  private static Field getField(Class<?> clazz, String fieldName)
+      throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    Field field = clazz.getDeclaredField(fieldName);
+    field.setAccessible(true);
+    return field;
+  }
+
+  private static Method getMethod(Class<?> clazz, String methodName)
+      throws NoSuchMethodException {
+    for (Method method : clazz.getDeclaredMethods()) {
+      if (method.getName().equals(methodName)) {
+        method.setAccessible(true);
+        return method;
+      }
+    }
+    return null;
   }
 }
 
