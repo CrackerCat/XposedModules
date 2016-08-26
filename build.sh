dc53b47f1b482c1b23d28358f20b822e64b8769b
TheCjw <thecjw@live.com>
2016-08-26 11:21:00 +0800

    .

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 TERM=dumb
-ls ./*/gradlew | xargs dirname | xargs -I % sh -c 'cd %;chmod +x gradlew;./gradlew assembleDebug;'
+ls $(pwd)/*/gradlew | xargs dirname | xargs -I % sh -c 'cd %;chmod +x gradlew;./gradlew assembleDebug;'
